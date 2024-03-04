22c2c40d942c9e9eace178f0ca862aa9ba7a541c
hasanalay <hsn_aly@hotmail.com>
2024-03-04 14:37:34 +0300

    Refactor build.sh script

---
+++ robowizards/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+
+# echo "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
