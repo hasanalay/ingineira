438a578c385fc526489b012a856cd0262845efbd
hasanalay <hsn_aly@hotmail.com>
2024-03-26 23:38:03 +0300

    Delete unused images and templates

--- robowizards/website/models.py
+++ robowizards/website/models.py
@@ -1,43 +1,4 @@
 from django.db import models
-# I am going to change all models for my new index page later.
-
-class About(models.Model):
-    Title = models.CharField(max_length=200)
-    Description = models.TextField(max_length=200)
-    Created = models.DateField(auto_now_add=True)
-    Image = models.ImageField(upload_to='about_image/', null=True, blank=True) 
-    
-    def __str__(self):
-        return self.Title
-    
-class News(models.Model):
-    Title = models.CharField(max_length=200)
-    Description = models.TextField(max_length=200)
-    Created = models.DateField(auto_now_add=True)
-    Image = models.ImageField(upload_to='new_image/', null=True, blank=True) 
-    
-    def __str__(self):
-        return self.Title
-    
-class Contact(models.Model):
-    Name = models.CharField(max_length=100)
-    Email = models.EmailField(max_length=200)
-    Address = models.CharField(max_length=200, null=True, blank=True)
-    Phone = models.CharField(max_length=20, null=True, blank=True)
-    Linkedin = models.URLField(max_length=200, null=True, blank=True)
-    Created = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.Name
-    
-class Team(models.Model):
-    Name = models.CharField(max_length=50)
-    Description = models.TextField(max_length=200)
-    Created = models.DateField(auto_now_add=True)
-    Image = models.ImageField(upload_to='team_member_image/', null=True, blank=True)
-    
-    def __str__(self):
-        return self.Name
 
 class Service(models.Model):
     Title = models.CharField(max_length=200)
--- robowizards/website/views.py
+++ robowizards/website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from . models import About, News, Service, Team , Contact, Article
+from . models import  Service, Article
 
 def index(request):
     latest_articles = Article.objects.order_by('-Created')[:4]
@@ -9,43 +9,3 @@ def index(request):
         'services': latest_services
         
     })
-
-# def article (request):
-#     articles = Article.objects.all()
-#     return render(request, 'index.html', {
-#         'articles': articles
-#     })
-
-# def home(request):
-#     return render(request, 'home.html')
-
-# def about(request):
-#     abouts = About.objects.all()
-#     return render(request, 'about.html', {
-#         'abouts': abouts
-#     })
-    
-# def news(request):
-#     news = News.objects.all()
-#     return render(request, 'news.html', {
-#         'news': news
-#     })
-    
-# def services(request):
-#     services = Service.objects.all()
-#     return render(request, 'services.html', {
-#         'services': services
-#     })
-
-# def team(request):
-#     teams = Team.objects.all()
-#     return render(request, 'team.html', {
-#         'teams': teams
-#     })
-    
-# def contact(request):
-#     contacts = Contact.objects.all()
-#     return render(request, 'contact.html', {
-#         'contacts': contacts
-#     })
-    
